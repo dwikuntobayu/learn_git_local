45e138bc3eee6136b9cc185259676b3fc8416aa1
mohamad_dwikuntobayu <dwikunto.bayu@kiranatama.com>
2014-10-10 21:47:07 +0700

    modify request.host

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params_user)
+    @url_host = request.host
     if @user.save
       begin
         #HeroMailer.htm_mail("#{@user.email}", @user.activation_token).deliver
-        ConfirmationMailer.confirm_email("#{@user.email}", request.host, @user.activation_token).deliver
+        ConfirmationMailer.confirm_email("#{@user.email}", @url_host, @user.activation_token).deliver
       rescue
         flash[:notice] = "activation instruction fails send to your email"
       end
